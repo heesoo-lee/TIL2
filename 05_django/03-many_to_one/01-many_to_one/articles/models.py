27fd8d1384ceb6472c35c19e94129302113a1061
이희수 <leeheesoo0624@gamil.com>
2025-08-14 17:24:37 +0900

    노트 필기

---
+++ 05_django/03-many_to_one/01-many_to_one/articles/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class Node:
+    def __init__(self, value):
+        self.item = value
+        self.left = None
+        self.Rig
+
+# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=120)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    article = models.ForeignKey
