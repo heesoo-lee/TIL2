af3e88c1dba780fab76bdf5cbd9fd695e8001e25
이희수 <leeheesoo0624@gmail.com>
2025-10-02 17:51:52 +0900

    데이터분석

--- 999_algorithm/백준/11501_주식/11501_주식.py
+++ 999_algorithm/백준/11501_주식/11501_주식.py
@@ -7,10 +7,10 @@ t = int(input())
 #     answer = 0
 #
 #     for i in range(n):
-#         last = price[i:]
-#         if last[0] == max(last):
+#         last = price[i:] # 오늘 이후의 주식 가격들
+#         if last[0] == max(last): #오늘이 가장 비싸면 걍 지나가기
 #             continue
-#         answer += (max(last) - last[0])
+#         answer += (max(last) - last[0]) # 아니면 가장 비싼날 팔기
 #
 #     print(answer)
 
@@ -21,10 +21,10 @@ for _ in range(t):
     max_price = 0
     answer = 0
 
-    for i in range(n - 1, -1, -1):
-        if price[i] > max_price:
-            max_price = price[i]
+    for i in range(n - 1, -1, -1): # 거꾸로 탐색
+        if price[i] > max_price: #  오늘이 가장 비싼 날보다 비싸면
+            max_price = price[i] # 가장 비싼 날 업데이트
         else :
-            answer += (max_price - price[i])
+            answer += (max_price - price[i]) # 아니면 가장 비싼 날 팔기
 
     print(answer)
